e7f26f5a5a5dce50af5dd98952d24f8c375fc9ed
TJ Holowaychuk <tj@vision-media.ca>
2013-05-16 20:50:16 +0000

    example

--- example.js
+++ example.js
@@ -3,6 +3,6 @@ var Log = require('./');
 var log = new Log('backend-web');
 
 log.debug('removed');
-log.info('removed');
+log.info('add something', { id: 'whatever' });
 log.warn('something failed');
 log.error('remove file', new Error('got 404 response'));
